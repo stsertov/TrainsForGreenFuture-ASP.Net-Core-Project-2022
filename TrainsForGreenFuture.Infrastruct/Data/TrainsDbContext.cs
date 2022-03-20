dc8a5a18da66df95449b21614f81e28528925eae
Stoyan Sertov <stsrtoff@gmail.com>
2022-03-20 14:47:07 +0200

    Project Preparation

---
+++ TrainsForGreenFuture.Infrastruct/Data/TrainsDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace TrainsForGreenFuture.Data
+{
+    public class TrainsDbContext : IdentityDbContext
+    {
+        public TrainsDbContext(DbContextOptions<TrainsDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
